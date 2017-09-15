41670819f3aa64ac7db7dd335b78d61e08ad3c5a
RuJyi <rujyi94@hotmail.com>
2017-09-15 06:08:59 +0000

    Adopt PSR-2 coding style
    The Laravel framework adopts the PSR-2 coding style in version 5.1.
    Laravel apps *should* adopt this coding style as well. Read the
    [PSR-2 coding style guide][1] for more details and check out [PHPCS][2]
    to use as a code formatting tool.
    
    [1]: https://github.com/php-fig/fig-standards/blob/master/accepted/PSR-2-coding-style-guide.md
    [2]: https://github.com/squizlabs/PHP_CodeSniffer

--- app/Http/Middleware/SettingsMiddleware.php
+++ app/Http/Middleware/SettingsMiddleware.php
@@ -29,20 +29,20 @@ class SettingsMiddleware
         } else if ($mailer == 'sendgrid') {
             config()->set([
                 'mail.host' => get_setting('mail_sendgrid_host'),
-               // 'mail.driver' => $mailer,
+                'mail.driver' => 'smtp',
                 'mail.username' => get_setting('mail_sendgrid_username'),
                 'mail.password' => get_setting('mail_sendgrid_password'),
                 'mail.from.name' => get_setting('mail_from_name'),
                 'mail.from.address' => get_setting('mail_from_email'),
             ]);
-        } else if ($mailer == 'sparkpost'){
+        } else if ($mailer == 'sparkpost') {
             config()->set([
                 'mail.driver' => $mailer,
                 'services.mailgun.secret' => get_setting('mail_sparkpost_secret'),
                 'mail.from.name' => get_setting('mail_from_name'),
                 'mail.from.address' => get_setting('mail_from_email'),
             ]);
-        }else if ($mailer == 'smtp'){
+        } else if ($mailer == 'smtp') {
             config()->set([
                 'mail.host' => get_setting('mail_smtp_host'),
                 'mail.driver' => $mailer,
@@ -52,7 +52,7 @@ class SettingsMiddleware
                 'mail.encryption' => get_setting('mail_smtp_encryption'),
                 'mail.from.name' => get_setting('mail_from_name'),
                 'mail.from.address' => get_setting('mail_from_email'),
-          ]);
+            ]);
         }
 
 
