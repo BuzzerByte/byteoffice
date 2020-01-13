01c220a2c09b5c4d8d30a6f50b05ef1276054660
RuJyi <rujyi94@hotmail.com>
2020-01-14 00:08:27 +0800

    update with upstream master

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-namespace buzzeroffice\Providers;
-=======
 namespace App\Providers;
->>>>>>> 9364050604be82bb21bf77314501118a9268d954
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -15,11 +11,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-<<<<<<< HEAD
-        'buzzeroffice\Model' => 'buzzeroffice\Policies\ModelPolicy',
-=======
         'App\Model' => 'App\Policies\ModelPolicy',
->>>>>>> 9364050604be82bb21bf77314501118a9268d954
     ];
 
     /**
