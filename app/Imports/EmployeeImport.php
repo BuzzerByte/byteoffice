bd4712eaa6921118efdceb29f9e016168af8a144
RuJyi <rujyi94@hotmail.com>
2020-08-16 00:54:43 +0800

    update employee module

---
+++ app/Imports/EmployeeImport.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Imports;
+
+use App\User;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+class EmployeeImport implements ToModel, WithHeadingRow
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new User([
+            'name' => $row['first_name']." ".$row['last_name'],
+            'email' => $row['email'],
+            'f_name' => $row['first_name'],
+            'l_name' => $row['last_name'],
+            'marital_status'=> $row['marital_status'],
+            'dob'=> $row['dob'],
+            'id_number'=> $row['id_number'],
+            'gender'=> $row['gender'],
+            'country'=> '-',
+            'blood_group'=> '-',
+            'religious'=> '-',
+            'terminate_status'=> 0,
+        ]);
+    }
+}
