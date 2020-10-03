2f1495899e47146c2df041ff40d6360584d4e184
RuJyi <rujyi94@hotmail.com>
2020-10-03 21:37:14 +0800

    update employee status controller

--- resources/views/admin/employeestatus/delete.blade.php
+++ resources/views/admin/employeestatus/delete.blade.php
@@ -8,6 +8,7 @@
         </div>
         <form action="" id="form-d-empStatus" enctype="multipart/form-data" method="post">
             @csrf
+            @method('delete')
         <div class="modal-body">
                 Are you sure?
         </div>
