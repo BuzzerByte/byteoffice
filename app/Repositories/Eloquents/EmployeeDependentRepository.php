ef6f443d995437053ebe9385238e8aee1fd06abf
RuJyi <rujyi94@hotmail.com>
2020-10-16 17:50:32 +0800

    update employee dependent

--- app/Repositories/Eloquents/EmployeeDependentRepository.php
+++ app/Repositories/Eloquents/EmployeeDependentRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquents;
 
 use App\Repositories\Interfaces\IEmployeeDependentRepository;
 use App\EmployeeDependent;
+use Illuminate\Http\Request;
 
 class EmployeeDependentRepository implements IEmployeeDependentRepository{
     protected $employeeDependents;
@@ -18,5 +19,33 @@ class EmployeeDependentRepository implements IEmployeeDependentRepository{
 
     public function getDependentById($id){
         return $this->employeeDependents->where('employee_id',$id)->get();
+    }
+
+    public function store(Request $request){
+        $employeeDependent = $this->employeeDependents;
+        $employeeDependent->name = $request->name;
+        $employeeDependent->relationship = $request->relationship;
+        $employeeDependent->dob = $request->date_of_birth;
+        $employeeDependent->employee_id = $request->employee_id;
+        return [
+            'result' => $employeeDependent->save(),
+            'employeeDependent' => $employeeDependent
+        ];
+    }
+
+    public function update(Request $request, EmployeeDependent $employeeDependent){
+        $employeeDependent = $this->employeeDependents->find($employeeDependent->id);
+        $employeeDependent->name = $request->name;
+        $employeeDependent->relationship = $request->relationship;
+        $employeeDependent->dob = $request->date_of_birth;
+        return [
+            'result' => $employeeDependent->save(),
+            'employeeDependent' => $employeeDependent
+        ];
+    }
+
+    public function destroy($id){
+        $employeeDependent = $this->employeeDependents->find($id);
+        return $employeeDependent->delete();
     }
 }
